aabdbd9b62dec84010f749e76e1ac67f6d85f12a
angel roberto <angelsystem1387@gmail.com>
2012-04-26 22:03:19 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flisol::Application.config.secret_token = '0594d77c7a96d06d2fbfb6e50a213a0100b13b4b08d780c41beb47d2414612b6c60e1eb9aa390421c59ab4e986325f22f7fbdd4b51c8d0e05950217d177bffb9'
